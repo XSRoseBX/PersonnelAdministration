74d7d08ac83a606a2a1c239186333fc439337922
XSRoseBX <moushaya@kkg-do.eu>
2022-09-12 21:58:01 +0200

    Homework to the 13th of September

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,10 +1,19 @@
 public class MainProgram {
     public static void main (String[] args) {
+<<<<<<< HEAD
         Employee y = new Employee ("Max", "Mustermann", "01.01.2000", 'm', "Has cancer", 22, 65.5);
         System.out.println(y.getName());
         Employee y2 = y;
         y2.changeName ("Alex", "Something");
         System.out.println (y.getName());
         
+=======
+        Employee y = new Employee ();
+        y.changeName ("Max");
+        System.out.println(y.getName());
+        Employee y2 = y;
+        y2.changeName ("Alex");
+        System.out.println (y.getName());
+>>>>>>> e45e4f2 (Homework to the 13th of September)
     }
 }
