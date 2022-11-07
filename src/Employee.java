7ee333c8fe72e1f5e2fe218ba5326d3afdc76025
XSRoseBX <moushaya@kkg-do.eu>
2022-11-07 20:49:19 +0100

    Homework to the 8th of November

--- src/Employee.java
+++ src/Employee.java
@@ -1,46 +1,64 @@
 public class Employee {
+    // all attributes
     private String forename;
     private String surname;
     private String birthdate;
     private char gender;
-    private String takeNoticeOf;
+    private String special;
     private int age;
     private double weight;
     private double salary;
     private String department;
     private int yearsInService;
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public Employee (String forename, String surname, String birthdate, char gender, String takeNoticeOf, int age, double weight) {
+    // constructor
+    public Employee (String forename, String surname, String birthdate, char gender, String special, int age, double weight) {
         this.forename = forename;
         this.surname = surname;
         this.birthdate = birthdate;
         this.gender = gender;
         this.age = age;
         this.weight = weight;
-        this.takeNoticeOf = takeNoticeOf;
+        this.special = special;
         this.salary = 1500;
         this.department = "A38";
         this.yearsInService = 0;
     }
 
+    // All getters
     public String getName () { return forename + " " + surname; }
     public double getSalary() { return salary; }
     public String getForename() { return forename; }
     public String getSurname() { return surname; }
     public String getBirthdate() { return birthdate; }
     public char getGender() { return gender; }
-    public String getTakeNoticeOf() { return takeNoticeOf; }
+    public String special() { return special; }
     public int getAge() { return age; }
     public double getWeight() { return weight; }
     public String getDepartment() { return department; }
     public int getYearsInService() { return yearsInService; }
 
-    public void changeName (String forename, String surname) {
-        this.forename = forename;
-        this.surname = surname;
+    // all Setters
+    public void setForename(String forename) { this.forename = forename; }
+    public void setSurname(String surname) { this.surname = surname; }
+    public void setBirthdate(String birthdate) { this.birthdate = birthdate; }
+    public void setGender(char gender) { this.gender = gender; }
+    public void setTakeNoticeOf(String special) { this.special = special; }
+    public void setAge(int age) { this.age = age; }
+    public void setWeight(double weight) { this.weight = weight; }
+    public void setSalary(double salary) { this.salary = salary; }
+    public void setYearsInService(int yearsInService) { this.yearsInService = yearsInService; }
+    public void setDepartment(String department) { this.department = department; }
+    public void setName (String forename, String surname) { this.forename = forename; this.surname = surname; }
+
+    //get all the needed info
+    public void getInfo () {
+        System.out.println (
+                "Surname: " + this.getSurname() +
+                ", Age: " + this.getAge() +
+                ", Gender: " + this.getGender() +
+                ", Salary: " + this.getSalary() +
+                ", Weight: " + this.getWeight()
+        );
     }
 }
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,15 +1,50 @@
+import java.lang.Math;
 public class MainProgram {
-    public static void main (String[] args) {
-        Employee e1 = new Employee ("Max", "Mustermann", "01.01.2000", 'm', "", 0, 65.5);
-        Employee e2 = new Employee ("Maya", "Musterfrau", "24.10.1976", 'f', "", 0, 65.5);
-        Employee e3 = new Employee ("Any", "Name", "06.05.2003", 'd', "", 0, 65.5);
-        Employee e4 = e1;
-        e1.setDepartment("C64");
-        e2.setDepartment("C64");
-
-        System.out.println (e1.getDepartment());
-        System.out.println (e2.getDepartment());
-        System.out.println (e3.getDepartment());
-        System.out.println (e4.getDepartment());
-    }
+        public static void main (String[] args) {
+                // homework, task 2a)
+                Employee e1 = new Employee(
+                        "Max", "Mustermann", "01.01.1982",
+                        'm', "Day off on every Monday", 40, 65.5
+                );
+                Employee e3 = new Employee(
+                        "Kiyan", "Jannati", "01.01.2008",
+                        'f', "Promotion in January", 14, 50
+                );
+                Employee e4 = new Employee(
+                        "Jasmina", "Gerhardt", "23.07.2007",
+                        'f', "Nothing of importance", 15, 55
+                );
+                Employee e5 = new Employee(
+                        "Maridiat", "Musterfrau", "09.10.2007",
+                        'm', "Gets raise soon", 15, 55
+                );
+                Employee e6 = new Employee(
+                        "Mattea", "Rukic", "14.04.2006",
+                        'f', "Slacking off a bit", 16, 55
+                );
+                Employee e2 = new Employee(
+                        "Kay", "Herchenröder", "24.10.1970",
+                        'd', "Nothing of importance", 52, 55
+                );
+                // homework, task 2b)
+                Employee[] employees = {e1, e2, e3, e4, e5, e6};
+                for (int i = 0; i < 6; i++) {
+                        employees[i].getInfo();
+                }
+                //homework task 2c)
+                for (int i = 0; i < 6; i++) {
+                        // homework task 2c.i)
+                        if (employees[i].getAge() >= 40) {
+                                employees[i].setSalary(3000);
+                        }
+                        // homework task 2c.ii)
+                        if (employees[i].getAge() >= 50) {
+                                employees[i].setSalary(4000 + (Math.random()) * 5000);
+                                // homework task 2c.iii)
+                                if (employees[i].getAge() >= 60) {
+                                        employees[i].setSalary(employees[i].getSalary() + 500);
+                                }
+                        }
+                }
+        }
 }
